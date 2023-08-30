c382127813bc468eba260190f4642a9d7167c1bf
Taro <ttte8kv9@gmail.com>
2023-08-30 09:05:43 +0900

    first commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "`user`")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+
+	/**
+	 * ユーザー名
+	 */
+	private String userName;
+
+	/**
+	 * ユーザー住所
+	 */
+	private String userAddress;
+
+	/**
+	 * ユーザーメールアドレス
+	 */
+	private String userEmailAddress;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserAddress() {
+		return userAddress;
+	}
+
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+
+	public String getUserEmailAddress() {
+		return userEmailAddress;
+	}
+
+	public void setUserEmailAddress(String userEmailAddress) {
+		this.userEmailAddress = userEmailAddress;
+	}
+
+}
